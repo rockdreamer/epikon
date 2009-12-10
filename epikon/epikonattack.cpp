267d81230b62ca224a88d059eec56efff9295955
rockdreamer <rockdreamer@e80e8944-e057-11de-b950-c1e2c078a2c5>
2009-12-10 16:27:15 +0000

    stylesheet on main window

--- epikon/epikonattack.cpp
+++ epikon/epikonattack.cpp
@@ -5,18 +5,21 @@
 #include "epikonshipitem.h"
 #include "epikongamescene.h"
 #include <QPropertyAnimation>
+#include <QSequentialAnimationGroup>
 #include <QDebug>
-
+#include <cmath>
 EpikonAttack::EpikonAttack(EpikonGameScene* scene, EpikonPlanet* from,
                            EpikonPlanet* to, EpikonPlayer* player, int numShips, QObject* parent):
     QParallelAnimationGroup(parent), numShips(numShips),m_to(to),m_player(player)
 {
+    int speed=4;
+    int duration = sqrt(pow(to->x()-from->x(),2)+pow(to->y()-from->y(),2))*speed;
     this->numShips=numShips;
     item = new EpikonShipItem(numShips);
     scene->addItem(item);
     QPropertyAnimation* anim;
     anim = new QPropertyAnimation(item,"pos");
-    anim->setDuration(1000);
+    anim->setDuration(duration);
     QPointF pos =from->item()->pos();
     anim->setStartValue(pos);
     pos=to->item()->pos();
@@ -24,6 +27,21 @@ EpikonAttack::EpikonAttack(EpikonGameScene* scene, EpikonPlanet* from,
     anim->setEasingCurve(QEasingCurve::InBack);
     addAnimation(anim);
 
+    QSequentialAnimationGroup *scalegroup = new QSequentialAnimationGroup(this);
+    anim = new QPropertyAnimation(item,"scale");
+    anim->setDuration((4*duration)/10);
+    anim->setStartValue(0);
+    anim->setEndValue(1);
+//    anim->setEasingCurve(QEasingCurve::OutCubic);
+    scalegroup->addAnimation(anim);
+    anim = new QPropertyAnimation(item,"scale");
+    anim->setDuration((6*duration)/10);
+    anim->setStartValue(1);
+    anim->setEndValue(0);
+    anim->setEasingCurve(QEasingCurve::InQuart);
+    scalegroup->addAnimation(anim);
+
+    addAnimation(scalegroup);
     connect(anim,SIGNAL(finished()),this,SLOT(onFinished()));
 }
 
